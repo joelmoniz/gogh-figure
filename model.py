cd921e748fc90f5c66724f32ea57edaf9f2a77d3
joelmoniz <jramoniz@gmail.com>
2016-12-11 22:05:04 +0530

    Add train dataset-related class

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ import numpy as np
 import theano
 import theano.tensor as T
 import lasagne
+import h5py
 
 from lasagne.layers import Conv2DLayer as ConvLayer
 from lasagne.layers import Pool2DLayer as PoolLayer
@@ -130,3 +131,38 @@ class Network(object):
 	def style_loss(self, out_layer, target_style_layer):
 		# Each input is a 4D tensor: (batch, feature map, height, width)
 		return T.mean(T.sum(T.sqr(self.batched_gram(out_layer) - T.tile(self.batched_gram(target_style_layer), (T.shape(out_layer)[0], 1, 1))), axis=(1,2)), axis=0)
+
+class CocoData(object):
+
+	PREPARE_COCO_SCRIPT_NAME = ''
+
+	def __init__(self, h5_location=None, train_batchsize=16, valid_batchsize=16):
+		if h5_location == None:
+			h5_location = REPO_DIR + "data/images/content/ms-coco-256.h5"
+		if not path_exists(h5_location):
+			print("Please download the COCO dataset, run the " +
+				self.PREPARE_COCO_SCRIPT_NAME + " and ensure that the hdf5 file is at " +
+				h5_location)
+
+		self.dataset = h5py.File(h5_location, "r")
+		self.train_batchsize = train_batchsize
+		self.valid_batchsize = valid_batchsize
+
+	def iterate_minibatches(self, inputs, batchsize, shuffle=False):
+		if shuffle:
+			indices = np.arange(len(inputs))
+			np.random.shuffle(indices)
+		for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+			if shuffle:
+				excerpt = list(indices[start_idx:start_idx + batchsize])
+				excerpt.sort()
+			else:
+				excerpt = slice(start_idx, start_idx + batchsize)
+
+			yield inputs[excerpt]
+
+	def get_train_batch(self):
+		return self.iterate_minibatches(self.dataset['train2014']['images'], self.train_batchsize, True)
+
+	def get_valid_batch(self):
+		return self.iterate_minibatches(self.dataset['val2014']['images'], self.valid_batchsize, False)
